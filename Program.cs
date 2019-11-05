d820d96f83282cdbf0fe993a44f3e6f497283afd
IT1050-IsaacMpoya <36143852+IT1050-IsaacMpoya@users.noreply.github.com>
2019-11-04 22:15:54 -0500

    Add files via upload
    Lab2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{// Start pf Class Hello world from Isaac Mpoya
+    class Program
+    { // Main method begins execution of C# app
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.WriteLine("from Isaac Mpoya");
+            Console.ReadLine();
+        }     
+    } /* End of main*/
+} /*End of class Hello World from Isaac Mpoya*/
